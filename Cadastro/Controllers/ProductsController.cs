4133b3d658467caeacc2bdf7604552c065311176
Carlos Augusto <junior.rg27@gmail.com>
2022-10-27 11:41:20 -0300

    Primeiro Commit

---
+++ Cadastro/Controllers/ProductsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cadastro.Infrastructure.Data.Common;
+using Cadastro.ViewModels;
+
+namespace Cadastro.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly RegisterContext _context;
+
+        public ProductsController(RegisterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductViewModel.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = await _context.ProductViewModel
+                .FirstOrDefaultAsync(m => m.IdProduto == id);
+            if (productViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(productViewModel);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProduto,Name,Value,Available")] ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productViewModel);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = await _context.ProductViewModel.FindAsync(id);
+            if (productViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(productViewModel);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProduto,Name,Value,Available")] ProductViewModel productViewModel)
+        {
+            if (id != productViewModel.IdProduto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductViewModelExists(productViewModel.IdProduto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productViewModel);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = await _context.ProductViewModel
+                .FirstOrDefaultAsync(m => m.IdProduto == id);
+            if (productViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(productViewModel);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productViewModel = await _context.ProductViewModel.FindAsync(id);
+            _context.ProductViewModel.Remove(productViewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductViewModelExists(int id)
+        {
+            return _context.ProductViewModel.Any(e => e.IdProduto == id);
+        }
+    }
+}
---
+++ Cadastro/ViewModels/ProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Cadastro;
+
+namespace Cadastro.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O código é requerido.")]
+        public int IdProduto { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome é requerido.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Valor")]
+        [Required(ErrorMessage = "O sobrenome é requerido.")]
+        public int Value { get; set; }
+
+        [Display(Name = "Disponível")]
+        public bool Available { get; set; }
+
+        [ForeignKey("EntidadeEstrangeira")]
+        [Display(Name = "Nome")]
+        public ClientViewModel Id { get; set; }
+    }
+}
